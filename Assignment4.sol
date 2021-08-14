e20430b11eeabb0ed207813ef0afd66c784962aa
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2021-08-14 09:17:06 +0100

    added assignment 4 solution

---
+++ Assignment4.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Assignment4 {
+  function crucial1(address a, address b, address c) public pure returns(bytes32 crucial1Hash) {
+    bytes32 tempHash = keccak256(abi.encodePacked(a, b));
+    crucial1Hash = keccak256(abi.encodePacked(tempHash, c));
+  }
+    
+  function crucial2(uint _num, bytes32 _value) public pure returns(bytes32 finalHash) {
+    address _first = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address _second = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    address _third = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+    bytes32 crucial1Hash = crucial1(_first, _second, _third);
+    
+    bytes32 numHash = keccak256(abi.encodePacked(_num));
+    uint value_ = uint(_value);
+    bytes32 valueHash = keccak256(abi.encodePacked(value_));
+    finalHash = keccak256(abi.encodePacked(numHash, valueHash, crucial1Hash));
+  }
+}
